50ad9f433d55971518e96eaf8d361f99eb025718
jonandonigv <jonandonigv@gmail.com>
2026-03-21 15:24:11 +0100

    typo on previous tab

--- lua/jon/core/keymaps.lua
+++ lua/jon/core/keymaps.lua
@@ -21,5 +21,5 @@ keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }
 keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab"})
 keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "close current tab"})
 keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" })
-keymap.set("n", "leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab"})
+  keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab"})
 keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab"})
